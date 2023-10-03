760ea325c12f068c9fe0991f7bf3825163f8429b
TrippyGaurav <gaurav.trippybug@gmail.com>
2023-10-03 16:00:22 +0530

    video removed from server

--- ytdownload/urls.py
+++ ytdownload/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     # Add your existing URL patterns for download_audio and download_video here
     path("download_audio/", views.download_audio, name="download_audio"),
     path("download_video/", views.download_video, name="download_video"),
+    path("delete_video/", views.download_video, name="delete_video"),
+
+    
 ]
 
 
