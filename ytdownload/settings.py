5200f1f2907e979b255ebce236f7c5ec12b934e6
unknown <karan.trippybug@gmail.com>
2023-10-05 15:18:07 +0530

    addede hosts

--- ytdownload/settings.py
+++ ytdownload/settings.py
@@ -10,7 +10,8 @@ SECRET_KEY = "django-insecure-xag%uymb+!gd8$x-pd=x9!hw=e-z4lir&ep1imm8er*(33p9^#
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
+
 
 
 # Application definition
